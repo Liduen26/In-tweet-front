8fddaba3b646925f8bf5a479f3a52ea6c9c4203e
JB <82393153+JB-0037@users.noreply.github.com>
2025-01-31 16:36:49 +0100

    Update login_empty_credentials.py

--- test/login_empty_credentials.py
+++ test/login_empty_credentials.py
@@ -2,12 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located, all_of
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 from dotenv import dotenv_values
 
 API_URL = dotenv_values("../.env").get("API_URL")
 
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 
 login = f'{API_URL}/login'
 firstError = "Le nom d'utilisateur ne doit pas être vide"
